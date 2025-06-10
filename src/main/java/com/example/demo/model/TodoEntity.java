2209856734649160bff8559d063650fcb5207963
dmswl6310 <dmswl6310@gmail.com>
2025-06-10 15:41:11 +0900

    feat: DTO와 Entity 작성

---
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder //= 오브젝트 생성(생성자 대체)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // 맴버변수의 getter, setter 메서드 구현
+public class TodoEntity {
+    private String id;  // 이 오브젝트의 아이디
+    private String userId; // 오브젝트 생성 유저 id
+    private String title; // 운동하기
+    private boolean done; // 완료한경우 true
+}

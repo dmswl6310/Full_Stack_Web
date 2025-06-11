38c93a24782a2cd4b69fcf4e4727c79016f8039f
dmswl6310 <dmswl6310@gmail.com>
2025-06-11 10:20:39 +0900

    feat: Create 기능 추가

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,27 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     @Autowired
     private TodoRepository repository;
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        validate(entity);
+
+        repository.save(entity);
+        log.info("Entity Id : {} is saved.",entity.getId());
+        return repository.findByUserId(entity.getUserId());
+    }
+    
+    private void validate(final TodoEntity entity){
+        if(entity==null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if(entity.getUserId()==null){
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user");
+        }
+    }
+
     public String testService(){
         // TodoEntity 생성
         TodoEntity entity=TodoEntity.builder().title("My first todo item").build();

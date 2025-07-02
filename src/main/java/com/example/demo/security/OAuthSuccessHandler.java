85364966556b4a5900f155f97436cb8f07a26ab2
dmswl6310 <dmswl6310@gmail.com>
2025-07-02 17:07:33 +0900

    feat: authorize 주소로 요청왔을때 쿠키 추가, 이후 프론트로 리다이렉트시 사용

--- src/main/java/com/example/demo/security/OAuthSuccessHandler.java
+++ src/main/java/com/example/demo/security/OAuthSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.security;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -9,26 +10,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 @Slf4j
 @AllArgsConstructor
 public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-//private final TokenProvider tokenProvider;
+    public static final String REDIRECT_URI_PARAM="redirect_url";
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        log.info("auth succeded");
         TokenProvider tokenProvider=new TokenProvider();
         String token=tokenProvider.create(authentication);
-        try {
-            response.setContentType("text/plain;charset=UTF-8");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().write("{\"token\": \"" + token + "\"}");
-//            response.getWriter().flush();
-        }catch(Exception e){
-            log.error("Failed to write token to response",e);
-        }
+
+        Optional<Cookie> oCookie= Arrays.stream(request.getCookies()).filter(cookie->cookie.getName().equals(REDIRECT_URI_PARAM)).findFirst();
+        Optional<String> redirectUri=oCookie.map(Cookie::getValue);
+
         log.info("token {}", token);
+//        response.setContentType("text/plain;charset=UTF-8");
+//        response.setStatus(HttpServletResponse.SC_OK);
+        response.sendRedirect(redirectUri.orElseGet(()->REDIRECT_URI_PARAM)+"/sociallogin?token="+token);
     }
 
 }
